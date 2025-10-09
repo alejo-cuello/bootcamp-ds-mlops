6fc4c25f7baffe710be1a756beaea5d5b6be79c8
alejo-cuello <alejocuello.ib@gmail.com>
2025-10-08 22:23:52 -0300

    [update] avances en main.py y call-api.py

--- 07-api/call-api.py
+++ 07-api/call-api.py
@@ -1,12 +1,20 @@
 import requests
 
-endpoint = "" # A completar
+endpoint = "prediccion"
 
 search_api_url = "http://127.0.0.1:8000/" + endpoint
 
 data = {
-    "campo_ejemplo": 1
-} # A completar
+        'ph': 0,
+        'Hardness': 204.890455,
+        'Solids': 20791.318981,
+        'Chloramines': 7.300212,
+        'Sulfate': 368.516441,
+        'Conductivity': 564.308654,
+        'Organic_carbon': 10.379783,
+        'Trihalomethanes': 86.990970,
+        'Turbidity': 2.963135,
+}
 
 response = requests.post(search_api_url, json=data, timeout=10)
 
--- 07-api/main.py
+++ 07-api/main.py
@@ -8,22 +8,43 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-MODEL_PATH = "" # Completar
+MODEL_PATH = "07-api/modelo/rf.pkl"
+with open(MODEL_PATH, "rb") as f:
+    model = pickle.load(f)
+  
+COLUMNS_PATH = "07-api/modelo/data_columns.pickle"
+with open(COLUMNS_PATH, 'rb') as handle:
+    ohe_tr = pickle.load(handle)
 
-# Completar
-# with open(MODEL_PATH, "rb") as f:
-#     model = pickle.load(f)
-    
+BINS_PH_PATH = '07-api/modelo/saved_bins_ph.pickle'
+with open(BINS_PH_PATH, 'rb') as handle:
+    new_saved_bins_ph = pickle.load(handle)
+
+BINS_SULFATE_PATH = '07-api/modelo/saved_bins_sulfate.pickle'
+with open(BINS_SULFATE_PATH, 'rb') as handle:
+    new_saved_bins_sulfate = pickle.load(handle)
+
+BINS_TRIHALOMETHANES_PATH = '07-api/modelo/saved_bins_trihalomethanes.pickle'
+with open(BINS_TRIHALOMETHANES_PATH, 'rb') as handle:
+    new_saved_bins_trihalomethanes = pickle.load(handle)
+
 class Answer(BaseModel):  # Completar
-    campo_ejemplo: float
-    # Add more fields as needed
+    ph: float
+    Hardness: float
+    Solids: float
+    Chloramines: float
+    Sulfate: float
+    Conductivity: float
+    Organic_carbon: float
+    Trihalomethanes: float
+    Turbidity: float
 
 @app.get("/")
 async def root():
     return {"message": "Grupo 02 levantando su primera API"}
 
-@app.post("/")
-def predict(answer: Answer): # Completar
+@app.post("/prediccion")
+def predict_water_potability(answer: Answer): # Completar
     return {"message": "Completar"} # Completar
 
 if __name__ == "__main__":
