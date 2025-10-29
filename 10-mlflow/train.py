a1dc5b9dd6f31ef3dfa06d991bd80986fd4f91f2
alejo-cuello <alejocuello.ib@gmail.com>
2025-10-29 20:15:19 -0300

    [update] mlflow en train.py

--- 10-mlflow/train.py
+++ 10-mlflow/train.py
@@ -1,12 +1,21 @@
+import mlflow.sklearn
 import pandas as pd
-import pickle
 
+from mlflow.models.signature import infer_signature
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score 
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier 
 
+# Parámetros a probar
+test_size = 0.3
+n_estimators = 500
+random_state_rf = 42
+
+# Setear experimento ML Flow
+mlflow.set_experiment(experiment_name="proyecto_bootcamp_jueves")
 
 # 1) Cargamos los datos
-data = pd.read_csv("../ejercicios-de-ejemplo/06/predictive_maintenance.csv", sep=',')
+data = pd.read_csv("ejercicios-de-ejemplo/06/predictive_maintenance.csv", sep=',')
 
 # 2) Preparación de la data
 data = data.drop(['UDI', 'Product ID', 'Failure Type'], axis=1)
@@ -23,36 +32,46 @@ data.rename(
 
 data = pd.get_dummies(data)
 
+
 # 3) Clasificación
 data_x = data.drop('Target', axis=1)
 data_y = data['Target']
 
-x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.3)
+x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=test_size)
 
-# Creamos 500 decision trees
-rf = RandomForestClassifier(n_estimators = 500, random_state = 42)
-
+# Creamos el modelo y lo entrenamos
+rf = RandomForestClassifier(n_estimators = n_estimators, random_state = random_state_rf)
 rf.fit(x_train, y_train)
 
 # Matriz de confusión
-y_pred1=rf.predict(x_train)
-              # Ajuste tamaño de letra (var global)
-conf_mat1=pd.crosstab(index=y_train,    # filas = valor real
-                     columns=y_pred1,   # columnas = valor predicho
-                     rownames=['Actual'], 
-                     colnames=['Pred'], 
-                     normalize='index')
+y_train_pred = rf.predict(x_train)
+y_test_pred = rf.predict(x_test)
 
-print(conf_mat1)
+# Métricas
+accuracy_tr = accuracy_score(y_train, y_train_pred)
+accuracy_test = accuracy_score(y_test, y_test_pred)
 
-# 4) Guardar el modelo
+# Guardar parámetros solicitados
+mlflow.log_param("Tamaño dataset", data_x.shape)
+mlflow.log_param("Porcentaje test", test_size)
+mlflow.log_param("Número de estimadores", n_estimators)
+mlflow.log_param("Random state rf", random_state_rf)
+mlflow.log_param("Accuracy train", accuracy_tr)
+mlflow.log_param("Accuracy test", accuracy_test)
+
+# # 4) Guardar el modelo
 filename = 'rf.pkl'
-pickle.dump(rf, open(filename, 'wb')) # rf = nuestro modelo
 
-# Lo cargamos para usarlo en otro momento. 
-rf_loaded = pickle.load(open(filename, 'rb'))
+signature = infer_signature(x_train, rf.predict(x_train))
 
-# 5) Guardar el nombre de las columnas
-# Guardamos las columnas x (sin Target)
-with open('categories_ohe.pickle', 'wb') as handle:
-    pickle.dump(data_x.columns, handle, protocol=pickle.HIGHEST_PROTOCOL)
+columns = data.columns[:-1]
+# x_train_df = pd.DataFrame(x_train, columns=columns)
+# input_example = x_train_df.head(5)
+
+mlflow.sklearn.log_model(
+    sk_model=rf,
+    name="random_forest_model",
+    signature=signature,
+    # input_example=input_example
+    # registered_model_name='predictive_maintenance_rf_model'
+)
