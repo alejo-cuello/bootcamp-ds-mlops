726f5f9c255601618b7d829a92984124397e73f2
alejo-cuello <alejocuello.ib@gmail.com>
2025-10-30 19:46:32 -0300

    [add] call model by request

---
+++ 10-mlflow/call_model_by_request.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import requests
+
+"""
+Recordar levantar el server, por ejemplo en el puerto 1234 de la siguiente manera:
+mlflow models serve --model-uri "runs:/<TU RUN ID>/<NOMBRE DE TU MODELO>" -p 8000 --no-conda
+"""
+
+# URL
+url = "http://127.0.0.1:8000/invocations"
+
+# Body data
+# Air_temperature	= 298.9
+# Process_temperature	= 309.1
+# Rotational_speed = 2861
+# Torque = 4.6
+# Tool_wear = 143
+# Type_H = 1 
+# Type_L = 0 
+# Type_M = 0
+
+# aux_data = [[Air_temperature, Process_temperature, Rotational_speed, Torque, Tool_wear, Type_H, Type_L, Type_M]]
+
+aux_data = [{
+    "Air_temperature": 298.9,
+    "Process_temperature": 309.1,
+    "Rotational_speed": 2861,
+    "Torque": 4.6,
+    "Tool_wear": 143,
+    "Type_H": 1,
+    "Type_L": 0,
+    "Type_M": 0
+}]
+
+request_data = {
+    "dataframe_records": aux_data
+}
+
+response = requests.post(url, json=request_data, timeout=10)
+print(response.json( ))
